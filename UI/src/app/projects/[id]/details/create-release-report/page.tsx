216bca559affa36f0a6e8422d6117b0c379b2f97
saurabh_farswan <saurav.farswan@xcdify.com>
2024-10-26 12:38:34 +0530

    Delete UI directory

--- UI/src/app/projects/[id]/details/create-release-report/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Page() {
-  return <div>Create Release Report</div>;
-}
-
-export default Page;
--- UI/src/app/projects/[id]/details/module-list/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Page() {
-  return <div>Module List</div>;
-}
-
-export default Page;
